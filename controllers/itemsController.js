74405b679797d663d6d35cf2644039f5fb8a1518
Cristhiam Fernández <cristhiam@MacBook-Pro-de-Cristhiam.local>
2025-03-23 00:32:25 -0500

    Agrega métodos get al API

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -14,3 +14,28 @@ exports.deleteItem = async (req, res) => {
         res.status(500).json({ message: 'Error al eliminar el artículo', error: err.message });
     }
 };
+
+exports.getItem = async (req, res) => {
+    console.log("item");
+    try {
+        const { id } = req.params;
+        const item = await Item.findAll({ where: { id } });
+        if (item) {
+            res.json(item)
+        } else {
+            res.status(404).json({ message: 'Artículo no encontrado'});
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Error al consultar artículo', error: err.message });
+    }
+};
+
+exports.getItems = async (req, res) => {
+    console.log("items");
+    try {
+        const items = await Item.findAll();
+        res.json(items)
+    } catch (err) {
+        res.status(500).json({ message: 'Error al consultar artículo', error: err.message });
+    }
+};
--- routes/items.js
+++ routes/items.js
@@ -5,6 +5,43 @@ const itemsController = require('../controllers/itemsController');
 /**
  * @swagger
  * /api/items/{id}:
+ *   get:
+ *     summary: Obtiene un artículo por ID
+ *     tags:
+ *       - Items
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: ID del artículo a obtener
+ *     responses:
+ *       200:
+ *         description: Artículo encontrado
+ *       404:
+ *         description: Artículo no encontrado
+ *       500:
+ *         description: Error del servidor
+ */
+router.get('/', itemsController.getItems);
+/**
+ * @swagger
+ * /api/items/:
+ *   get:
+ *     summary: Obtiene todos los artículos
+ *     tags:
+ *       - Items
+ *     responses:
+ *       200:
+ *         description: Todos los artículos
+ *       500:
+ *         description: Error del servidor
+ */
+router.get('/:id', itemsController.getItem);
+/**
+ * @swagger
+ * /api/items/{id}:
  *   delete:
  *     summary: Eliminar un artículo por ID
  *     tags:
